bce430988a2a0ce87785323ac12ad131d054f55b
Bruce Royce <brucerroyce@gmail.com>
2024-06-11 06:41:03 +0100

    feat: adding arc SVG transformer

---
+++ src/transform/hooks/useWarp.tsx
@@ -0,0 +1,50 @@
+import { useState, useRef, useEffect, type RefObject } from "react";
+import Warp from "warpjs";
+
+export default function useWarp({
+  svgRef,
+}: {
+  svgRef: RefObject<SVGSVGElement>;
+}) {
+  const [svgData, setSvgData] = useState<Record<string, number>>({
+    minX: Number.MAX_VALUE,
+    minY: Number.MAX_VALUE,
+    maxX: Number.MIN_VALUE,
+    maxY: Number.MIN_VALUE,
+    width: 1000,
+    height: 200,
+    vGap: 200,
+  });
+  const resetData = useRef<number[][]>([]);
+  const warpRef = useRef<any>(null);
+
+  useEffect(() => {
+    warpRef.current = new Warp(svgRef.current);
+    if (!warpRef.current) {
+      return;
+    }
+    const tempData = Object.assign({}, svgData);
+    resetData.current = [];
+    warpRef.current.transform(([x, y]: number[]) => {
+      resetData.current.push([x, y]);
+      tempData.minX = Math.min(tempData.minX, x);
+      tempData.minY = Math.min(tempData.minY, y);
+      tempData.maxX = Math.max(tempData.maxX, x);
+      tempData.maxY = Math.max(tempData.maxY, y);
+      return [x, y];
+    });
+    tempData.width = tempData.maxX - tempData.minX;
+    tempData.height = tempData.maxY - tempData.minY;
+    setSvgData(tempData);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function warpReset() {
+    const resetHolder = resetData.current.slice();
+    warpRef.current.transform(() => {
+      return resetHolder.shift();
+    });
+  }
+
+  return { svgData, warpRef, warpReset };
+}
--- src/transform/index.tsx
+++ src/transform/index.tsx
@@ -1,21 +1,60 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+import { ReactComponent as SampleText } from "@/../../.resources/sampleText.svg";
+import useWarp from "./hooks/useWarp";
+import "./transform.scss";
 
 /**
  * page for transforming the SVG
  */
+
 function Transform() {
-  const [value, setValue] = useState(0);
+  const svgRef = useRef<SVGSVGElement>(null);
+  const [value, setValue] = useState(50);
+  const { svgData, warpRef, warpReset } = useWarp({ svgRef });
+  const handleResetButton = () => {
+    warpReset();
+    setValue(50);
+  };
+
+  useEffect(() => {
+    warpReset();
+    const arcRad = (value - 50) * 1.2;
+    warpRef.current.transform(([x, y]: number[]) => {
+      const newOne = [x, y - arcRad * Math.sin(x / (1000 / 8))];
+      return newOne;
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [value]);
 
   return (
-    <div>
+    <div className="transform">
       <h1>Transform</h1>
-      <p>Please show the transformed SVG from sampleText.svg here</p>
+
+      <SampleText
+        ref={svgRef}
+        width={svgData.width}
+        height={svgData.height + svgData.vGap}
+        viewBox={`${svgData.minX} ${svgData.vGap / -8} ${svgData.width} ${
+          svgData.height + svgData.vGap / 2
+        }`}
+      />
+      <p>Transforming SVG from sampleText.svg</p>
       <input
         type="range"
         value={value}
         onChange={(event) => setValue(parseInt(event.target.value))}
       />
-      <p>{value}</p>
+      <p className="rangeValue">{value}</p>
+      <button className="btn" onClick={handleResetButton}>
+        Reset
+      </button>
+      <div className="vSpacer" />
+      <p className="sign">
+        Tech Test for Kittl.com by Bruce Royce - 2024 -{" "}
+        <a className="link" href="mailto:bruceroyce@yahoo.com">
+          bruceroyce@yahoo.com
+        </a>
+      </p>
     </div>
   );
 }
