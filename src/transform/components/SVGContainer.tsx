c80b78c296041c86ae3ed5e37d3613e851e05402
Bruce Royce <brucerroyce@gmail.com>
2024-06-11 06:41:03 +0100

    feat: part 2 adding arc SVG transformer

---
+++ src/transform/components/SVGContainer.tsx
@@ -0,0 +1,26 @@
+import { ReactComponent as SampleText } from "@/../../.resources/sampleText.svg";
+import { forwardRef, LegacyRef } from "react";
+
+type SVGContainerProps = {
+  width: number;
+  height: number;
+  viewBox: string;
+  reset: number;
+};
+
+const SVGContainer = forwardRef(function (
+  props: SVGContainerProps,
+  ref: LegacyRef<SVGSVGElement>
+) {
+  console.log("SVGContainer renered");
+  return (
+    <SampleText
+      ref={ref}
+      width={props.width}
+      height={props.height}
+      viewBox={props.viewBox}
+    />
+  );
+});
+
+export default SVGContainer;
